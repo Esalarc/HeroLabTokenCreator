6d7a4e484297954107cc967d1cb63f97d1561a85
dfedo <dfedo@192.168.3.163>
2016-09-19 20:48:34 -0400

    11: Store recent directories Preferences
    Task-Url: https://github.com/Esalarc/HeroLabTokenCreator/issues/11

--- src/com/fidosoft/por2tok/ui/handlers/OnFileGenerateToken.java
+++ src/com/fidosoft/por2tok/ui/handlers/OnFileGenerateToken.java
@@ -1,6 +1,7 @@
 package com.fidosoft.por2tok.ui.handlers;
 
 import java.io.File;
+import java.util.prefs.Preferences;
 
 import com.fidosoft.por2tok.HeroLabTokenCreator;
 import com.fidosoft.por2tok.Token;
@@ -19,11 +20,15 @@ public class OnFileGenerateToken implements EventHandler<ActionEvent>{
   @Override
   public void handle(ActionEvent event) {
     DirectoryChooser chooser = new DirectoryChooser();
-    File startIn = new File(System.getProperty("user.home"));
-    chooser.setInitialDirectory(startIn);
+    Preferences preferences = Preferences.userNodeForPackage(HeroLabTokenCreator.class);
+    String initialDir = preferences.get("file.generateToken.initialDir", System.getProperty("user.home"));
+
+    chooser.setInitialDirectory(new File(initialDir));
     File selected = chooser.showDialog(application.getStage());
-    if (selected != null && selected.exists())
+    if (selected != null && selected.exists()){
+      preferences.put("file.generateToken.initialDir", selected.getAbsolutePath());
       Token.generateTokens(selected, application.getPortfolio());
+    }
   }
 
 }
